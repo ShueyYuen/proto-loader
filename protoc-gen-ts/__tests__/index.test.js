09fbe7eb0ba07341f446cfa97a2018a778b925bb
ShueyYuen <2641176308@qq.com>
2025-01-01 22:53:44 +0800

    feat: 实现request读取

--- protoc-gen-ts/__tests__/index.test.js
+++ protoc-gen-ts/__tests__/index.test.js
@@ -3,17 +3,17 @@ const cp = require("node:child_process");
 const protoc = require("../../protoc");
 const fs = require("node:fs");
 
-const PLUGIN = path.resolve(__dirname, "../protoc-gen-ts");
+const PLUGIN = path.resolve(__dirname, "../target/debug/protoc-gen-ts");
 
 test("is protoc installed!", (done) => {
   const resultDir = path.resolve(__dirname, "./pb");
   const dataDir = path.resolve(__dirname, "../../examples");
   const dataFile = path.resolve(dataDir, "simple.proto");
 
-  if (fs.existsSync(resultDir)) {
-    fs.rmSync(resultDir, { recursive: true });
-  }
-  fs.mkdirSync(resultDir, { recursive: true });
+  // if (fs.existsSync(resultDir)) {
+  //   fs.rmSync(resultDir, { recursive: true });
+  // }
+  // fs.mkdirSync(resultDir, { recursive: true });
 
   protoc(
     [
--- protoc-gen-ts/src/main.rs
+++ protoc-gen-ts/src/main.rs
@@ -1,8 +1,15 @@
+use prost::Message;
+use prost_types::compiler::{CodeGeneratorRequest, CodeGeneratorResponse};
 use std::io::{self, Read};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = Vec::new();
     io::stdin().read_to_end(&mut buf)?;
     println!("{:?}", buf);
+
+    let requests =
+        CodeGeneratorRequest::decode(&buf[..]).expect("failed to decode CodeGeneratorRequest");
+
+    println!("{:?}", requests);
     Ok(())
 }
